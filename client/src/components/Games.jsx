695d73585fede65f63a5a74f04467d1567db1794
Luciano Petrate <lucianopetrate@gmail.com>
2023-05-18 23:42:13 -0300

    GameCard component added

--- client/src/components/Games.jsx
+++ client/src/components/Games.jsx
@@ -1,12 +1,14 @@
 import { useGames } from "../hooks/useGames";
+import GameCard from "./GameCard";
 
 function Games() {
   const { data } = useGames();
 
   return (
-    <ul className="text-center">
+    <ul className="text-center grid grid-cols-3 gap-2">
       {data?.map((game) => (
-        <li key={game.id}>{game.name}</li>
+        //<li key={game.id}>{game.name}</li>
+        <GameCard name={game.name}/>
       ))}
     </ul>
   );
